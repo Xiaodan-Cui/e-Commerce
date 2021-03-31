9ef2a3a742f3f5c285758181412e4e85d575365f
xiaodan cui <cxd@xiaodan-MacBook-Pro.local>
2021-03-31 10:09:06 -0700

    start

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/xiaodan/ecommerce/DAO/ProductCategoryRepository.java
@@ -0,0 +1,11 @@
+package xiaodan.ecommerce.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import xiaodan.ecommerce.entity.ProductCategory;
+@RepositoryRestResource(collectionResourceRel = "productCategory",path="product-category")
+@CrossOrigin("http://localhost:4200")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
